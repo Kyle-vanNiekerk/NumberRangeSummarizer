0737c99be008ba5e36cd03634c86be53f729e1f1
Kyle-van-Niekerk-578224 <578224@student.belgiumcampus.ac.za>
2023-04-21 20:25:06 +0200

    Implemented Interface

--- ReadFile.java
+++ ReadFile.java
@@ -1,8 +1,13 @@
 import java.io.File;
+import java.util.Collection;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
-public class ReadFile {
+public class ReadFile /*implements NumberRangeSummarizer*/ {
+    // Error whith summarizeCollection function, change parameter type
 
+    // Open a specified text file in a specified directory to read the input from
     public String getInput(String filename) {
         String pathname = filename + ".txt";
         String result = "";
@@ -18,14 +23,54 @@ public class ReadFile {
             myReader.close();
         
         }   catch (Exception e) {
-                System.out.println("File 'input.txt' does not exist");
+                System.out.println("File '"+ filename +".txt' does not exist");
             }
     
             return result;
         }
 
+    // Splits a string by delimiter ","
     public String [] splitByComma(String series){
         String[] result = series.split(",");
+        return result;
+    }
+
+    // Splits a string by dilimeter "," and stores data in an ArrayList Collection
+    public ArrayList<Integer> collect(String input){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        String[] series = splitByComma(input);
+
+        for (int i = 0; i < series.length; i++) {
+        result.add(Integer.parseInt(series[i]));
+        }
+        return result;
+    }
+
+    // Summarizes the collection
+    public String summarizeCollection(ArrayList<Integer> input){        
+        String result = "";
+
+        for (int i = 0; i < input.size()-1; i++) {
+            if ((input.get(i+1) == input.get(i)+1) /*&& (i+1 < input.size())*/) // Possible out of bounds error
+            {
+                String temp = input.get(i).toString() + "-";
+                int sequential = 0;
+                while (input.get(i) + sequential == input.get(i + sequential))
+                {
+                    sequential++;
+                }
+                result += temp + Integer.toString(sequential + (input.get(i) - 1));
+                i+= sequential-1;
+            }
+            else{
+                result += input.get(i).toString();
+            }
+            
+            if (i < input.size()-1)
+            result += ", ";
+
+        }
+        result += input.get(input.size()-1).toString(); // should be part of the loop
         return result;
     }
 }
--- app.java
+++ app.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class app {
     public static void main(String[] args) {
         
@@ -7,14 +9,17 @@ public class app {
 
         // Read From Specified File
         String inputLine = rf.getInput("input");
-        System.out.println(inputLine);
+        //System.out.println(inputLine);
 
         // Split line by delimiter "," into an array
         String[] splitSeries = rf.splitByComma(inputLine);
         for (int i = 0; i < splitSeries.length; i++) {
-            System.out.println(splitSeries[i]);
+            //System.out.println(splitSeries[i]);
         }
         
+        // collect series
+        ArrayList<Integer> seriesIntegers = rf.collect(inputLine);
+        System.out.println(rf.summarizeCollection(seriesIntegers));
     }    
 }
 
